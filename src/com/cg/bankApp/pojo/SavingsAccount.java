e68d303b0bc19c77c3493632909754403aec222b
JohariGourav <41551799+JohariGourav@users.noreply.github.com>
2018-08-13 08:19:46 +0530

    banking app (some modules)

---
+++ src/com/cg/bankApp/pojo/SavingsAccount.java
@@ -0,0 +1,18 @@
+package com.cg.bankApp.pojo;
+
+public class SavingsAccount {
+	
+	
+	private boolean isSalaried;
+	private int balance;
+	
+	
+	public SavingsAccount( boolean isSalaried, int balance) {
+		super();
+		
+		this.isSalaried = isSalaried;
+		this.balance = balance;
+	}
+	
+	
+}
